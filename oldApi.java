6d7d4af97346f96fdcfea54a9adde924000086ea
dautofreitas <dauto_freitas@hotmail.com>
2024-11-14 13:39:28 -0300

    Update and rename newApi.java to oldApi.java

---
+++ oldApi.java
@@ -0,0 +1,8 @@
+/**
+ * <b>NOTE</b>: Responses from this API are based upon the legacy Shiro permissions system.
+ * 
+ * @deprecated This API has been re-implemented using the Spring Framework. Please use
+ *             {@linkcom.test.ok.spring.newApi} instead.
+ */
+public class oldApi {
+}
